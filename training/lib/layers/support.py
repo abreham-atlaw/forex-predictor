b21635fde7b3448b50fdc9c89305a73271db0203
Abreham Atlaw <abishatlaw@gmail.com>
2021-07-02 13:41:39 +0300

    Added BiasLayer

--- training/lib/layers/support.py
+++ training/lib/layers/support.py
@@ -39,8 +39,23 @@ class PositionalEncoding(Layer):
 		super(PositionalEncoding, self).__init__(**kwargs)
 
 	def call(self, inputs, **kwargs):
-		# inputs.shape[1] must be an even number
+		# inputs.shape[2] must be an even number
 		r = tf.cast(tf.reshape(tf.range(inputs.shape[1]), (-1, 1)), tf.float64) / (
 				10000 ** (2 * tf.reshape(tf.range(inputs.shape[2]), (1, -1)) / inputs.shape[2]))
 		return tf.cast(inputs, tf.float64) + tf.reshape(tf.concat([tf.reshape(tf.sin(r[:,::2]), (r.shape[0], -1, 1)), tf.reshape(tf.sin(r[:,1::2]), (r.shape[0], -1, 1))], axis=2), r.shape)
 
+
+class BiasLayer(Layer):
+
+	def __init__(self, **kwargs):
+		super(BiasLayer, self).__init__(**kwargs)
+
+	def build(self, input_shape):
+		self.b = self.add_weight(name="bias",
+								shape=(1,),
+								trainable=True,
+								initializer="zeros"
+								)
+
+	def call(self, inputs, **kwargs):
+		return inputs + self.b
