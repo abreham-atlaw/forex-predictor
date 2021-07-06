d8b8d39e7ca032422dd366ebdfcf4bc9b17a8843
Abreham Atlaw <abishatlaw@gmail.com>
2021-07-06 19:36:12 +0300

    Public Repo Clean Up

--- core/Config.py
+++ core/Config.py
@@ -5,35 +5,9 @@ from training.lib.layers import Delta
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-print(BASE_DIR)
 
-POLYGON_KEY = "1ijeQ0XUYNl1YMHy6Wl_5zEBtGbkipUP"
+POLYGON_KEY = "123456789098765432" 
 INFORMATION_COLLECTOR_SLEEP_TIME = 60 #IN SECONDS
-
-BASE_CURRENCY = "USD"
-
-CURRENCIES = sorted([
-	"AUD",  # Australia
-	"BRL",  # Brazil
-	"CAD",  # Canada
-	"CNH",  # Chine
-	"DKK",  # Denmark
-	"EUR",  # Euro
-	"HKD",  # Hong Kong
-	"JPY",  # Japan
-	"NZD",  # New Zealand
-	"NOK",  # Norway
-	"SGD",  # Singapore
-	"ZAR",  # South Africa
-	"KRW",  # South Korea
-	"SEK",  # Sweden
-	"CHF",  # Switzerland
-	"TWD",  # Taiwan
-	"GBP"   # United Kingdom
-])
-
-MODEL_PATH = os.path.join(BASE_DIR, "models/model0.h5")
-MODEL_LOOK_BACK = 50
 
 PREDICTORS_CONFIGS = [
 	{
